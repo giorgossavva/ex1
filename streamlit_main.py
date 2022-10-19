260f482f9b76133bfba29102873bacd2abfbd78a
giorgossavva <81297220+giorgossavva@users.noreply.github.com>
2022-10-19 16:55:46 +0300

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,1 +1,11 @@
-hey
+import streamlit as st
+
+PAGES = {
+    "About us": existing_data
+}
+
+st.set_page_config(page_title="Covid19-Cyprus", page_icon="🧊", layout='wide', initial_sidebar_state='auto')
+st.sidebar.title('🧭 Navigation')
+selection = st.sidebar.radio("", list(PAGES.keys()))
+page = PAGES[selection]
+page.streamlit_app()
